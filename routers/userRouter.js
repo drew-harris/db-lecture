e26e4c8e93a08aa747c735fadd69bb7953a8c04d
Drew Harris <harrisd@smu.edu>
2023-02-15 21:12:38 -0600

    Hash password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 
 const userRouter = express.Router();
 
@@ -7,6 +8,21 @@ userRouter.get("/", (req, res) => {
   return res.json({
     message: "Hello from user router",
   });
+});
+
+userRouter.post("/signup", async (req, res) => {
+  console.log("signup request");
+  console.log(req.body);
+
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(req.body.password, salt);
+  console.log(hash);
+
+  return res.json({ message: "User Created" });
 });
 
 export { userRouter };
