3aac4186d6f0cb1b696564faa549ebdfb2475281
Drew Harris <harrisd@smu.edu>
2023-02-15 20:59:38 -0600

    Setup prisma and json

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import { userRouter } from "./routers/userRouter.js";
 
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Recieved request");
