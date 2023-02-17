7b5a61ae3f0537c8eb225d1d1019216e821f6bd1
Drew Harris <harrisd@smu.edu>
2023-02-17 09:50:58 -0600

    Add login route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -12,6 +12,45 @@ userRouter.get("/", (req, res) => {
   });
 });
 
+userRouter.post("/login", async (req, res) => {
+  try {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({ message: "bad request" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "unauthorized" });
+    }
+
+    // eslint-disable-next-line
+    const { password, ...userWithoutPassword } = user;
+
+    const token = jwt.sign(userWithoutPassword, process.env.JWT_SECRET, {
+      expiresIn: "12d",
+    });
+
+    return res.json({ jwt: token });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 userRouter.post("/signup", async (req, res) => {
   try {
     console.log("signup request");
@@ -34,6 +73,7 @@ userRouter.post("/signup", async (req, res) => {
       },
     });
 
+    // eslint-disable-next-line
     const { password, ...rest } = user;
 
     const token = jwt.sign(rest, process.env.JWT_SECRET, {
